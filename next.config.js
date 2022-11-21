6f695bab12885ddbd2dd7c8eac433d06037675a8
Ariq N <111544927+ariqnrnns@users.noreply.github.com>
2022-11-21 16:16:48 +0000

    Initial files upload

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+
+module.exports = {
+  images: {
+    domains: [
+      'images.unsplash.com'
+    ],
+		deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+  },
+}
+
